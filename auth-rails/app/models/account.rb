8ca1b98c7b4984ca8fb098f708b86f23176797ad
Artjoms Juferovs <artjoms.juferovs@gmail.com>
2021-03-15 15:18:14 +0200

    Return account.rb

---
+++ auth-rails/app/models/account.rb
@@ -0,0 +1,21 @@
+class Account < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+
+  has_many :access_grants,
+         class_name: 'Doorkeeper::AccessGrant',
+         foreign_key: :resource_owner_id,
+         dependent: :delete_all # or :destroy if you need callbacks
+
+  has_many :access_tokens,
+         class_name: 'Doorkeeper::AccessToken',
+         foreign_key: :resource_owner_id,
+         dependent: :delete_all # or :destroy if you need callbacks
+
+  before_create do
+    self.public_id = SecureRandom.uuid
+  end
+end
